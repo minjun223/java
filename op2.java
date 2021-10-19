8cec2b1439912a2e155f908b13a80371ae082a83
김민준 <80810346+minjun223@users.noreply.github.com>
2021-10-19 22:36:14 +0900

    Add files via upload

---
+++ op2.java
@@ -0,0 +1,17 @@
+package chap00;
+
+public class op2 {
+
+	public static void main(String[] args) {
+		int i=0, j=0;
+		String k = "*";
+		for(i=0; i<5; i++)
+		{
+			for(j=0; j<=i; j++)
+			{
+			System.out.print(k);
+			}System.out.print("\n");
+		}
+	}
+
+}
