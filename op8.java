8c35ea0148d40ef74cd8b4e771e2caf2d77e8258
김민준 <80810346+minjun223@users.noreply.github.com>
2021-10-28 20:04:46 +0900

    Add files via upload

---
+++ op8.java
@@ -0,0 +1,12 @@
+package chap00;
+
+public class op8 {
+
+//	public static void main(String[] args) {
+//		String c = input("Ã¶¼ö");
+//		String y = input("¿µÈñ");
+//		whoswin(c, y);
+//	}
+//	public static void main whoswin()
+
+}
