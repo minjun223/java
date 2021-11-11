4369fc393c4517bf5746104e95d7ece69effbf30
김민준 <80810346+minjun223@users.noreply.github.com>
2021-11-11 23:31:57 +0900

    Add files via upload

---
+++ EX_10.java
@@ -0,0 +1,18 @@
+package chap02;
+
+public class EX_10 {
+
+	public static void main(String[] args) {
+		echo("¾È³ç", 3);
+	}
+
+	private static void echo(String string, int i) {
+		int a;
+		for(a=0; a<i; a++)
+		{
+			System.out.println(string);
+		}
+		
+	}
+
+}
