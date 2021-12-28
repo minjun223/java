7592fde75266f7189db31ff94a251b7fbcd4e178
김민준 <80810346+minjun223@users.noreply.github.com>
2021-12-28 22:42:30 +0900

    Add files via upload

---
+++ Two.java
@@ -0,0 +1,12 @@
+package chap06;
+
+public class Two {
+	void print() {
+		one o = new one();
+		//System.out.println(secret);
+		System.out.println(o.roommate);
+		System.out.println(o.child);
+		System.out.println(o.anybody);
+	}
+
+}
