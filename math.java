ccb84ec3e79358ed8bc29e56495db5d6191b3fb4
김민준 <80810346+minjun223@users.noreply.github.com>
2021-12-31 08:51:13 +0900

    Add files via upload

---
+++ math.java
@@ -0,0 +1,18 @@
+package mine;
+
+public class math {
+
+	public static void main(String[] args) {
+		int [][] mathScore = new int[2][3];
+		for(int i=0; i<mathScore.length; i++) {
+			for(int k=0; k<mathScore[i].length; k++) {
+				System.out.println("mathScore["+i+"]["+k+"]="+mathScore[i][k]);
+			}
+		}
+		System.out.println();
+		
+//		int[][] englishScores = new int[2][];
+//		englishScores[0] = new int[2];
+	}
+
+}
