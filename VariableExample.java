fc80dfb49313806382c0dcbc2191fee1ba80379a
김민준 <80810346+minjun223@users.noreply.github.com>
2022-01-20 09:44:02 +0900

    Add files via upload

---
+++ VariableExample.java
@@ -0,0 +1,15 @@
+package mine;
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		int hour = 6;
+		int minute = 5;
+		System.out.println(hour + "½Ã°£ " + minute + "ºÐ");
+		
+		int totalMinute = (hour*60) + minute;
+		System.out.println("ÃÑ" + totalMinute + "ºÐ");
+
+	}
+
+}
