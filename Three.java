45de8da10d11f768b2687e820fd6ca4e9a452ee6
김민준 <80810346+minjun223@users.noreply.github.com>
2022-03-09 15:01:04 +0900

    Add files via upload

---
+++ Three.java
@@ -0,0 +1,16 @@
+package test1;
+
+import chap06.one;
+public class Three {
+	
+
+	void print(char[] anybody) {
+		one o = new one();
+		
+		//System.out.println(secret);
+		//System.out.println(roommate);
+		//System.out.println(child);
+		System.out.println(anybody);
+	}
+	
+}
