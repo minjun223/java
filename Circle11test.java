6773d4879571834386ed6188183bf00c592b84fb
김민준 <80810346+minjun223@users.noreply.github.com>
2022-03-12 11:27:37 +0900

    Add files via upload

---
+++ Circle11test.java
@@ -0,0 +1,13 @@
+package test1;
+
+import chap06.Circlemj;
+
+public class Circle11test {
+
+	public static void main(String[] args) {
+		Circlemj c1 = new Circlemj(5);
+
+		System.out.println(c1.findArea());
+	}
+
+}
