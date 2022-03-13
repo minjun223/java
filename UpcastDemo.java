78ba130d18b75267d5900797cdc0e5b7dcaab4f1
김민준 <80810346+minjun223@users.noreply.github.com>
2022-03-14 08:14:06 +0900

    Add files via upload

---
+++ UpcastDemo.java
@@ -0,0 +1,35 @@
+//package test1;
+//
+//import java.security.Permission;
+//
+//class person{
+//	String name = "사람";
+//	
+//	void whoami(){
+//		System.out.println("나는 사람이다.");
+//	}
+//}
+//class stud extends person{
+//	int number =7;
+//	
+//	void work() {
+//		System.out.println("나는 공부한다");
+//	}
+//}
+//public class UpcastDemo {
+//
+//	public static void main(String[] args) {
+//		//person p = new stud();
+////		person p;
+////		p=new stud();
+//		
+//		person p;
+//		stud s = new stud();
+//		
+//		p=s;
+//		
+//		//p.number = 1;
+//		//p.work();
+//	}
+//
+//}
