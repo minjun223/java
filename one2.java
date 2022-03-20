a7e64bb1f27bfba75ea5a8c29568f86e617918a8
김민준 <80810346+minjun223@users.noreply.github.com>
2022-03-21 07:59:58 +0900

    Add files via upload

---
+++ one2.java
@@ -0,0 +1,13 @@
+package test1;
+
+import chap06.one;
+
+public class one2 extends one{
+	void print() {
+		//System.out.println(secret);
+		//System.out.println(roommate);
+		System.out.println(child);
+		System.out.println(anybody);
+	}
+
+}
