9cd0f8ee235645e74eb3195708af5ca75562689c
김민준 <80810346+minjun223@users.noreply.github.com>
2022-03-31 07:52:06 +0900

    Add files via upload

---
+++ GirlTest2.java
@@ -0,0 +1,38 @@
+package _10_14;
+
+//class Girl{
+//	private String name;
+//	void show() {
+//		
+//	}
+//	public Girl(String name) {
+//		this.name=name;
+//	}
+//	public Girl() {
+//		
+//	}
+//}
+//class GoodGirl extends Girl{
+//	void show() {
+//		System.out.println("그녀는 자바를 잘안다");
+//	}
+//}
+//
+//class BestGirl extends GoodGirl{
+//	void show() {
+//		System.out.println("그녀는 자바를 무지하게 잘않다");
+//	}
+//}
+//public class GirlTest2 {
+//
+//	public static void main(String[] args) {
+//		Girl g1 = new Girl();
+//		Girl g2 = new GoodGirl();
+//		GoodGirl gg = new BestGirl();
+//		
+//		g2.show();
+//		gg.show();
+//
+//	}
+//
+//}
